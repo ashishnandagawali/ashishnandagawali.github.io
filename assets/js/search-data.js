0071f747279d30a78b12874b81d1c6fd553664b7
ashishnandagawali <ashish.nandagawali@gmail.com>
2025-02-01 19:34:26 +0000

    Deploying to gh-pages from @ ashishnandagawali/ashishnandagawali.github.io@bf37431b01c59593188e35bbacb7ac2a38798037 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,26 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/certifications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-image-galleries",
       
